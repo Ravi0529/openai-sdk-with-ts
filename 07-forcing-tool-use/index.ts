f840225b9fdf4f95232619f22728ed88b9249d6a
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-12-17 02:18:15 +0530

    forcing-tool-use

---
+++ 07-forcing-tool-use/index.ts
@@ -0,0 +1,22 @@
+import { Agent, tool } from "@openai/agents";
+import { z } from "zod";
+
+const calculatorTool = tool({
+  name: "Calculator",
+  description: "Use this tool to answer questions about math problems.",
+  parameters: z.object({
+    question: z.string(),
+  }),
+  execute: async (input) => {
+    throw new Error("TODO: implement this");
+  },
+});
+
+const agent = new Agent({
+  name: "Strict tool user",
+  instructions: "Always answer using the calculator tool.",
+  tools: [calculatorTool],
+
+  // "auto", "required", "none"
+  modelSettings: { toolChoice: "required" }, // it is on "auto" by default
+});
